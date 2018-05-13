9cd05efeb6c7f3810c5cfaee8640700c9f5a3416
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-05-13 22:47:54 +0300

    adeed ParkingService to model

---
+++ ParkingEmulator/ParkingService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ParkingEmulator
+{
+    static class ParkingServices
+    {
+
+        // Parking actions
+        static public string GetAllParkingPlace()
+        {
+            return Parking.Instance.ParkingSpace.ToString();
+        }
+
+        static public string GetFreeParkingPlace()
+        {
+            return Parking.Instance.FreeParkingSpace.ToString();
+        }
+
+        static public string GetParkingBalance()
+        {
+            return Parking.Instance.Balance.ToString();
+        }
+
+        // Transaction actions
+        static public string GetTransactionsLogFile()
+        {
+            var log = String.Empty;
+
+            try
+            {
+                log = File.ReadAllText(Settings.logFile);
+            }
+            catch (FileNotFoundException)
+            {
+                return "Error, file Transactions.log not found";
+            }
+
+            return "Transactions.log";
+        }
+
+        static public IEnumerable<Transaction> GetTransactionForLastMinute()
+        {
+            return Parking.Instance.GetTransactionsForLastMinute;
+        }
+    }
+}
